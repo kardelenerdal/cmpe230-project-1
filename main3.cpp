903c99a9703a0b3c6ba3ba60469ca73b8389c127
Kardelen Erdal <47900788+kardelenerdal@users.noreply.github.com>
2021-04-24 00:44:56 +0300

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -32,7 +32,7 @@ string inToPost(string infix) {
           postfix += " ";
           continue;
       }
-      if(isalnum(char(*it))){
+      if(isalnum(char(*it)) || *it == '%'){
          postfix += *it;      //add to postfix when character is letter or number
       }else if(*it == '(')
          stk.push('(');
@@ -115,22 +115,124 @@ string spaceCheck(string str){
 // line = " choose(choose(523), choose(), a, b) "
 // line = "choose("%t6", choose(), a,b)"
 // recline= "choose(523)" "%t6"
-string chooseFinder(string line){
-   
+vector<string> chooseParser(string a){
+  //cout << "chooseline"<< a<<endl;
+    int insertPoint = a.find_last_of(")");
+    a.insert(insertPoint, ",");
+    //cout << "a"<<a<<endl;
+    vector<string> parList;
+    
+    bool firstTime = true;
+    int nofParenthes = 0;
+    int virgulIndex = 0;
+    int lastIndex = 0;
+    int firstIndex = 0;
+    int len = 0;
+    for(int i = 0; i < a.length(); i++){
+      if(string(1, a[i]) == "(" && firstTime == true){
+          firstTime = false;
+           firstIndex = i + 1;
+           continue;
+       }
+       if(string(1, a[i]) == "(" && firstTime == false){
+           nofParenthes++;
+       } 
+      if(string(1, a[i]) == ")" && firstTime == false){
+           nofParenthes--;
+       }
+      if(string(1, a[i]) == "," && nofParenthes == 0 && firstTime == false){
+        virgulIndex = i;
+        lastIndex = virgulIndex - 1;
+        len = lastIndex - firstIndex + 1;
+        //cout <<"pushladim"<<(a.substr(firstIndex, len))<<endl;
+        parList.push_back(a.substr(firstIndex, len));
+        firstIndex = virgulIndex + 1;
+      }
+    }
+    return parList;
 }
+
+string choose(string line){
+  vector<string> parameters = chooseParser(line);
+  //for(int i = 0; i < parameters.size(); i++){
+    //cout << parameters[i]<<"k"<<endl;
+  //}
+    //if(parameters[i].find("choose") != string::npos){
+      //choose(parameters[i]);
+    //} else {
+      string par1Name = expression(false, parameters[0]);
+      string firstConditionName = "%t" + to_string(nofTempVariables);
+      afterAllocation << firstConditionName << " = icmp eq i32 " << par1Name << ", 0" << endl;
+      nofTempVariables++;
+      
+      string par2Name = expression(false, parameters[1]);
+      string secondConditionName = "%t" + to_string(nofTempVariables);
+      afterAllocation << secondConditionName << " = icmp sgt i32 " << par1Name << ", 0" << endl;
+      nofTempVariables++;
+      //cout << parameters[2]<<endl;
+      string par3Name = expression(false, parameters[2]);
+      //cout << par3Name<<endl;
+      string par4Name = expression(false, parameters[3]);
+      //cout << par4Name<<endl;
+      string elseName = "%t" + to_string(nofTempVariables);
+      afterAllocation << elseName << " = select i1 " << secondConditionName << ", i32 " << par3Name << ", i32 " << par4Name << endl;
+      nofTempVariables++;
+      string chooseName = "%t" + to_string(nofTempVariables);
+      afterAllocation << chooseName << " = select i1 " << firstConditionName << ", i32 " << par2Name << ", i32 " << elseName << endl;
+      nofTempVariables++;
+
+      return chooseName;
+    //}
+  }
+   // ?  expressiona mı atılacak?
+   //   %1 = alloca i32, align 4
+   //   %2 = alloca i32, align 4
+   //   store i32 0, i32* %1, align 4
+   //   %3 = call i32 @choose(i32 0, i32 1, i32 2, i32 3)
+   //   store i32 %3, i32* %2, align 4
+   //   ret i32 0
 
 // // choose(0,1,2,3) + 5
 std::vector<std::string> expressionParser(bool equal, string line){  
    // while choose varsa
    // choose fonksiyonuna line'ı at
    // line bana ilk choose u halletmiş bir şekilde yeni line'ı vercek
-   
+  while(line.find("choose") != string::npos){
+//cout << line <<endl;
+    string chooseName = choose(line);
+    // choose yerine bunukoycam
+    int chooseFirstIndex = line.find("choose");
+    int chooseLastIndex = 0;
+    bool inChoose = false;
+    int nofParantheses = 0;
+    for(int i=0; i<line.length(); i++){
+      if(i == chooseFirstIndex){
+        inChoose = true;
+      }
+      if(inChoose){
+        if(line[i] == '('){
+          nofParantheses++;
+        } else if(line[i] == ')'){
+          nofParantheses--;
+          if(nofParantheses == 0){
+            chooseLastIndex = i;
+            break;
+          }
+        }
+      }
+    }
+    //cout << line<<endl;
+    string result = line.substr(0, chooseFirstIndex) + " " + chooseName + " " + line.substr(chooseLastIndex+1);
+    //cout << result<<endl;
+    line = result;
+  }
+
    // sonuç olarak choose suz yeni bir line'ım var 
    // line = "a = choose(3,4,(a-5),6) + 6 + choose(0,1,2,3)"
    // line = "a = %t8 + 6 + choose(0,1,2,3)"
    // line = "a = %t8 + 6 + %t50"
     line = spaceCheck(line);
-    
+    //cout << line << endl;
     int l = line.length();
     char str[l+1];
     strcpy(str, line.c_str());
@@ -281,6 +383,7 @@ string expression(bool equal, string expr) {
   // postfixe çevirme
   for(int i=postfixVector.size()-1; i>0; i--) {   // ilk indexi almadım çünkü o left variable
       postfixVersion.push(postfixVector[i]);
+      //cout << postfixVector[i]<<endl;
   }
   // postfixVersion = a2x*5z-/+2y/+  a'dan başlarayak çıkıcak.
   
@@ -300,25 +403,27 @@ string expression(bool equal, string expr) {
 
       string token = postfixVersion.top();
       postfixVersion.pop();
-
-      if(isVariable(token) || isNumber(token)){
+      if(isVariable(token) || isNumber(token) || token[0] == '%'){
       //cout << "variable"<<token<<endl;
         waitList.push(token);
 
       } else if(isOperator(token)){
-
+//cout << token <<endl;
         string var1 = waitList.top();
+        //cout << var1<<endl;
         string var1temp = "";
         waitList.pop();
         string var2 = waitList.top();
+       // cout << var2<<endl;
         string var2temp = ""; 
         waitList.pop();
 
         var1temp = handleVariable(var1);
         var2temp = handleVariable(var2);
+        //cout << var1temp<<endl;
 
         if(token == "+"){
-
+//cout << "arti"<<token<<endl;
           string resultName = "%t" + to_string(nofTempVariables);            //nofTempVar nerde increment oluyor?
           afterAllocation << resultName << " = add i32 " << var2temp << ", " << var1temp << endl;
           waitList.push(resultName);
@@ -369,6 +474,7 @@ string fixLine(string line){
       int commentIndex = line.find("#");
       line = line.substr(0,commentIndex);
   }
+
   int startOfLine = 0;
   int endOfLine = 0;
   for (int i=0; i<line.length(); i++) {
@@ -379,11 +485,13 @@ string fixLine(string line){
   }
    for (int i=line.length()-1; i>=0; i--) {
         if (!isspace(line[i])){
+          endOfLine = i;
           break;
         }
-        endOfLine = i;
-    }
-  return line.substr(startOfLine, endOfLine - startOfLine);
+        //endOfLine = i;
+  }
+  //cout << endOfLine << " asdad"<< startOfLine;
+  return line.substr(startOfLine, endOfLine - startOfLine + 1);
 }
 
 int main(int argc, char const *argv[]) {
@@ -406,7 +514,7 @@ int main(int argc, char const *argv[]) {
 
    // comment and spaces bunda errorlara da bakabiliriz
     line = fixLine(line);
-    
+  
     if(line.find("print")!= string::npos) { // içinde statement ya da choose varsa hesapla yoksa direkt yaz 
 
       int startIndex = line.find("print") + 6;
@@ -433,7 +541,7 @@ int main(int argc, char const *argv[]) {
       isCondition = true;
 
     } else if(line.find('=')!= string::npos) {   // sağı hesapla sola koy
-     
+      
       expression(true, line);  
 
     }else if(line.find("}") != string::npos && isCondition == true){
